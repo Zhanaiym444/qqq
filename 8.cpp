2554a54e7270d7d07c6d49e0b15faec41ee068f3
Zhanaiym444 <zhanaiymzhagparova@gmail.com>
2025-06-17 17:53:24 +0500

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int sum=0;
     for (int i = 1; i <= 5; i++) {
-        sum +=i
-        cout << sum << " ";
+        sum +=i;
     }
+    cout << sum << " ";
     return 0;
 }
