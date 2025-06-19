0bc34e3db0148400be4086f04adde10e24f7044b
Zhanaiym444 <zhanaiymzhagparova@gmail.com>
2025-06-19 10:04:57 +0500

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -5,9 +5,14 @@ int main() {
     int day;
     cin >> day;
     switch(day) {
-        case 1: cout << "Mon"; break;
-        case 2: cout << "Tue"; break;
-        default: cout << "Other";
+        case 1: cout << "Monday"; break;
+        case 2: cout << "Tuesday"; break;
+        case 3: cout << "Wednesday"; break;
+        case 4: cout << "Thursday"; break;
+        case 5: cout << "Friday"; break;
+        case 6: cout << "Saturday"; break;
+        case 7: cout << "Sunday"; break;
+        default: cout << "Invalid day";
     }
     return 0;
 }
